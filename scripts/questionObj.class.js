beece4ab94d6b3fc65d63cefbb66792ada04a02e
Matthew Martin <43966328+mar17132@users.noreply.github.com>
2019-07-10 11:56:39 -0600

    api call update

---
+++ scripts/questionObj.class.js
@@ -0,0 +1,23 @@
+function questionObj()
+{
+    this.questionArray = [];
+    this.numberAnwsers = 3;
+    this.songsObj = null;
+}
+
+questionObj.prototype.getSongAnwsers = function(){
+    for(i = 0; i < this.numberAnwsers; i++)
+    {
+        song = this.songsObj.getSongRandom(this.songsObj.getArraySize());
+        song.setBeenUse(true);
+        this.questionArray.push(song);
+    }
+};
+
+questionObj.prototype.setSongsObj = function(songs){
+    this.songsObj = songs;
+};
+
+questionObj.prototype.setQuestionAnwser = function(){
+    this.songsObj.getSong(this.numberAnwsers).setAnwser(true);
+};
