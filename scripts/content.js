450878c8a1bab0315c60d2895a92cca7b1d996b5
Chris Carrick-2 <car17059@byui.edu>
2019-07-11 22:24:56 -0600

    added content.js, and working on gameScreen.html

---
+++ scripts/content.js
@@ -0,0 +1,84 @@
+splash = `
+<div id="splashMainDiv"></div>
+<div id="splashLogoContainer">
+    <div id="splashLogo">
+        <img src="images/logo.svg" alt="">
+    </div>
+</div>
+<div id="splashContinueButtonContainer">
+    <div id="splashContinueButton">
+        <button type="button">CONTINUE</button>
+    </div>
+</div>
+<div id="noRotate">
+    <h2>Please don't Rotate</h2>
+    <p>This game is best played in portrait mode.</p>
+</div>`
+
+home = `
+<div id="homeContainer">
+    <div id="homeInnerContainer">
+        <div id="homeLogo">
+            <img src="images/logo.svg" alt="" >
+        </div>
+        <div class="homeButton">
+            <button type="button">START GAME</button>
+        </div>
+        <div class="homeButton">
+            <button type="button">INSTRUCTIONS</button>
+        </div>
+        <div id="homeSetupHeader">
+            <h2>– setup –</h2>
+        </div>
+        <div>
+            <h3>Number of Questions</h3>
+        </div>
+        <div id="homeQuestionOptionButtons">
+            <button class="optionButton selected single">5</button>
+            <button class="optionButton">10</button>
+            <button class="optionButton">15</button>
+        </div>
+        <div>
+            <h3>Seconds to Guess</h3>
+        </div>
+        <div id="homeQuestionOptionButtons">
+            <button class="optionButton single">5</button>
+            <button class="optionButton selected">10</button>
+            <button class="optionButton">15</button>
+        </div>
+    </div>
+</div>
+<div id="homeContinueButtonContainer"></div>
+<div id="noRotate">
+    <h2>Please don't Rotate</h2>
+    <p>This game is best played in portrait mode.</p>
+</div>`
+
+instructions = `
+<div id="instructionsContainer">
+    <div id="instructionsInnerContainer">
+        <div id="instructionsLogo">
+            <img src="images/logo.svg" alt="" >
+        </div>
+        <div id="instructionsSetupHeader">
+            <h2>INSTRUCTIONS</h2>
+            <div id="instructionsRecordImage"></div>
+        </div>
+        <div class="instructionsListContainer">
+            <ul>
+                <li><strong>Step 1</strong>: Select the number of songs you would like to guess</li>
+                <li><strong>Step 2</strong>: Select the number of seconds you want to be able to guess</li>
+                <li><strong>Step 3</strong>: Click Start Game</li>
+                <li><strong>Step 4</strong>: Guess which artist sang the lyrics displayed</li>
+                <li><strong>Step 5</strong>: Continue making awsome guesses until your game is over</li>
+            </ul>
+        </div>
+    </div>
+</div>
+<div id="backButton">
+    <a href="#"><img src="images/BackButton.svg"></a>
+</div>
+<div id="noRotate">
+    <h2>Please don't Rotate</h2>
+    <p>This game is best played in portrait mode.</p>
+</div>`
