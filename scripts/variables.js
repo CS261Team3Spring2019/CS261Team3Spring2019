66b1b2eb2ac46235da9210f323ad36e48fd69cc4
Matthew Martin <43966328+mar17132@users.noreply.github.com>
2019-07-17 21:53:35 -0600

    api update

--- scripts/variables.js
+++ scripts/variables.js
@@ -1,4 +1,5 @@
 var myQuestions = null; //question varaible
 var hasLoaded = false; //To keep track of second thread completion
+var siteUser = null //user object variable
 let numQuestions = 5; // number of questions in game
 var currentQuestion = 1;
